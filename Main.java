d984ea6c979a3fab4f4e82c03b2e76167b751dbe
Wish49 <137053479+Wish49@users.noreply.github.com>
2023-06-19 13:05:19 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] arg){
+        // char [] exept = new char [3];
+      calc("");
+
+    }
+    public static String calc (String input){
+        String str = sc.nextLine();
+        String[] parts = str.split(" ");
+        int num1 = Integer.parseInt(parts[0]);
+        int num2 = Integer.parseInt(parts[2]);
+        if (num1 < 1 || num1 > 10){
+            System.out.println("Вы ввели не правильно!");
+            System.exit(0);
+        }
+        if (num2 < 1 || num2 > 10){
+            System.out.println("Вы ввели не правильно!");
+            System.exit(0);
+        }
+        String operStr = parts[1];
+        char[] oper= operStr.toCharArray();
+        int sum = 0;
+        if (oper[0] == '+'){
+            sum = num1 + num2;
+        }else if (oper[0] == '-') {
+            sum = num1 - num2;
+        } else if (oper[0] == '*') {
+            sum = num1 * num2;
+        } else if (oper[0] == '/') {
+            sum = num1 / num2;
+        }
+        System.out.println(sum);
+        String str2 = String.valueOf(sum);
+
+
+        return str2;
+    }
+}
